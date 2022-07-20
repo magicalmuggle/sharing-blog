26cec3068e1b2a4f092e6d24f424d0fd157c07de
magicalmuggle <magicalmuggle@foxmail.com>
2022-07-20 19:26:54 +0800

    Break the dependency cycle between beans

--- src/main/java/com/github/magicalmuggle/blog/configuration/WebSecurityConfig.java
+++ src/main/java/com/github/magicalmuggle/blog/configuration/WebSecurityConfig.java
@@ -1,24 +1,16 @@
 package com.github.magicalmuggle.blog.configuration;
 
-import com.github.magicalmuggle.blog.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.inject.Inject;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Inject
-    private UserService userService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // 防止 post 请求被拒绝
@@ -32,23 +24,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return authenticationManager();
     }
 
-    @Inject
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
-    }
-
     // 密码加密器
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setHideUserNotFoundExceptions(false);
-        provider.setUserDetailsService(userService);
-        provider.setPasswordEncoder(bCryptPasswordEncoder());
-        return provider;
     }
 }
